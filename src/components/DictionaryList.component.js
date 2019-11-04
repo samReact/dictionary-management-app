06e0815c31ea930648276a535cef1465ff9d3da4
tifus69 <tifus69@hotmail.com>
2019-11-04 19:39:44 +0100

    refactor dictionaryList create Dictionary row

--- src/components/DictionaryList.component.js
+++ src/components/DictionaryList.component.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Button, Row, Col } from 'reactstrap';
-import { MdDeleteForever, MdEdit, MdRemoveRedEye } from 'react-icons/md';
-import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Row, Col } from 'reactstrap';
 
-import { DELETE_DICTIONARY } from '../actions/types/dictionaries.action.type';
 import { ComponentWrapper } from '../styled/style';
-import { notify } from '../utils';
+import DictionaryRow from './DictionaryRow.component';
 
 const DictionaryList = () => {
   const dictionaries = useSelector(state => state.dictionariesReducer.dictionaries);
-  const dispatch = useDispatch();
-  let history = useHistory();
-  const handleRemove = id => {
-    const payload = { id };
-    dispatch({ payload, type: DELETE_DICTIONARY });
-    notify('success', 'Dictionary deleted !');
-  };
 
   return (
     <ComponentWrapper>
@@ -30,42 +20,7 @@ const DictionaryList = () => {
       </Row>
       <div style={{ overflowY: 'scroll' }}>
         {dictionaries.map(dictionary => (
-          <Row key={dictionary.id} className="mt-2">
-            <Col xs="4">
-              <h6>{dictionary.name}</h6>
-            </Col>
-            <Col xs="4">
-              <h6>{dictionary.rows.length}</h6>
-            </Col>
-            <Col xs={{ size: 1, offset: 1 }}>
-              <Button
-                onClick={() =>
-                  history.push({
-                    pathname: '/editDictionary',
-                    state: { id: dictionary.id },
-                  })
-                }
-                outline
-              >
-                <MdEdit />
-              </Button>
-            </Col>
-            <Col xs="1">
-              <Button
-                onClick={() =>
-                  history.push({ pathname: '/dictionary', state: { dictionary } })
-                }
-                outline
-              >
-                <MdRemoveRedEye />
-              </Button>
-            </Col>
-            <Col xs="1">
-              <Button color="danger" onClick={() => handleRemove(dictionary.id)} outline>
-                <MdDeleteForever />
-              </Button>
-            </Col>
-          </Row>
+          <DictionaryRow dictionary={dictionary} />
         ))}
       </div>
     </ComponentWrapper>
---
+++ src/components/DictionaryRow.component.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Button, Row, Col } from 'reactstrap';
+import { MdDeleteForever, MdEdit, MdRemoveRedEye } from 'react-icons/md';
+import { useHistory } from 'react-router-dom';
+
+import { DELETE_DICTIONARY } from '../actions/types/dictionaries.action.type';
+import { notify } from '../utils';
+import { IconWarning } from '../styled/style';
+
+const DictionaryRow = ({ dictionary }) => {
+  const hasDuplicate = dictionary.rows.map(row => row.hasDuplicate);
+  const dispatch = useDispatch();
+  let history = useHistory();
+  const handleRemove = id => {
+    const payload = { id };
+    dispatch({ payload, type: DELETE_DICTIONARY });
+    notify('success', 'Dictionary deleted !');
+  };
+  return (
+    <Row key={dictionary.id} className="mt-2">
+      <Col xs="4">
+        <h6>{dictionary.name}</h6>
+      </Col>
+      <Col xs="4">
+        <h6>{dictionary.rows.length}</h6>
+      </Col>
+      <Col xs="1" style={{ display: 'flex', alignItems: 'center' }}>
+        {hasDuplicate.length && <IconWarning />}
+      </Col>
+      <Col xs={{ size: 1 }}>
+        <Button
+          onClick={() =>
+            history.push({
+              pathname: '/editDictionary',
+              state: { id: dictionary.id },
+            })
+          }
+          outline
+        >
+          <MdEdit />
+        </Button>
+      </Col>
+      <Col xs="1">
+        <Button
+          onClick={() => history.push({ pathname: '/dictionary', state: { dictionary } })}
+          outline
+        >
+          <MdRemoveRedEye />
+        </Button>
+      </Col>
+      <Col xs="1">
+        <Button color="danger" onClick={() => handleRemove(dictionary.id)} outline>
+          <MdDeleteForever />
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
+export default DictionaryRow;
